8d1c2508d66de6efd55f8a972ff56167b6235b9c
u3shadow <u3shadow@gmail.com>
2018-04-27 14:50:41 +0800

    add android test use espresso

---
+++ app/src/androidTest/java/com/u3coding/mvvmtest/NewUserActivityTest.java
@@ -0,0 +1,25 @@
+package com.u3coding.mvvmtest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.u3coding.mvvmtest.view.NewUserActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class NewUserActivityTest {
+    @Rule
+    public ActivityTestRule<NewUserActivity> mActivityRule = new ActivityTestRule(NewUserActivity.class);
+    @Test
+    public void checkButtonIsShow(){
+        onView(withId(R.id.edit_word)).check(matches(isDisplayed()));
+    }
+}
